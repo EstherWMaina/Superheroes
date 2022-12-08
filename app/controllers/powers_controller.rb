cf9bee2c84aec2709e6b007ee82ab96f4f0498cd
Esther Maina <mainaesther77@gmial.com>
2022-12-08 13:45:47 +0300

    show power_controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,5 +2,13 @@ class PowersController < ApplicationController
     def index
         render json: Power.all
     end
-    def 
+
+    def show
+        power = Power.find_by(id: params[:id])
+        if power
+            render json: power
+        else  
+            render json: {error: "Power not found"}, status:
+            :not_found
+        end
 end
