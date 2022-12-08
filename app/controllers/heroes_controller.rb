6bc32068ca08135da42a74f1ba74d0a54718c11e
Esther Maina <mainaesther77@gmial.com>
2022-12-08 13:39:14 +0300

    Hero not found rendering

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -5,7 +5,12 @@ class HeroesController < ApplicationController
     
     def show 
         hero = Hero.find_by(id: params[:id])
+        if hero
         render json: hero
+        else 
+            render json: {error: "Hero not found"}, status:
+            :not_found
+      end
     end
     
 end
